95d10475d25d4c6023801035632e024b5ed1c37f
flpbbk <filip.biblekaj@outlook.de>
2023-09-11 18:37:06 +0200

    add feature TimeConvert

--- src/main/java/com/example/slmtest/Impl.java
+++ src/main/java/com/example/slmtest/Impl.java
@@ -1,4 +1,17 @@
 package com.example.slmtest;
 
 public class Impl {
+    public String time(int h)
+    {
+        if (h<=12)
+            return h+ " AM";
+        else
+            return h-12 + " PM";
+
+    }
+
+    public String info()
+    {
+        return "This API converts 24h time into AM/PM time.";
+    }
 }
